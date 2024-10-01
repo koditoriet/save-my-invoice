db06500af90c69bc3769b01146e924ab46ddb1d1
Anton Ekblad <anton@ekblad.cc>
2024-10-01 16:49:59 +0200

    Add @updateURL and @downloadURL tags.

--- save-my-invoice.user.js
+++ save-my-invoice.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name             Save my invoice!
 // @description      Disables non-billable fields in Fieldglass to avoid reporting accidents.
+// @updateURL        https://raw.githubusercontent.com/koditoriet/save-my-invoice/main/save-my-invoice.user.js
+// @downloadURL      https://raw.githubusercontent.com/koditoriet/save-my-invoice/main/save-my-invoice.user.js
 // @match            https://www.fieldglass.net/time_sheet_form.do*
 // @version          0.1
 // ==/UserScript==
