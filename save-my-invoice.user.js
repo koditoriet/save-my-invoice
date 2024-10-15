b4a05957bdb045315bb678fd23a82f37f20d6c5c
Anton Ekblad <anton@ekblad.cc>
2024-10-15 09:57:41 +0200

    Fix @match for post-migration Fieldglass.

--- save-my-invoice.user.js
+++ save-my-invoice.user.js
@@ -3,8 +3,8 @@
 // @description      Disables non-billable fields in Fieldglass to avoid reporting accidents.
 // @updateURL        https://raw.githubusercontent.com/koditoriet/save-my-invoice/main/save-my-invoice.user.js
 // @downloadURL      https://raw.githubusercontent.com/koditoriet/save-my-invoice/main/save-my-invoice.user.js
-// @match            https://www.fieldglass.net/time_sheet_form.do*
-// @version          0.1
+// @match            https://*.fieldglass.net/time_sheet_form.do*
+// @version          0.1.1
 // ==/UserScript==
 
 let es = document.querySelectorAll(".nonBillableRow + tr td input");
